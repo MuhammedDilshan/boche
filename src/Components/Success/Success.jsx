9956b0f63a602e29ab0bd11734740c115918ea06
hananpilakkadan <hanan.p@webandcrafts.com>
2024-05-22 22:39:46 +0530

    animation updated

--- src/Components/Success/Success.jsx
+++ src/Components/Success/Success.jsx
@@ -3,10 +3,12 @@ import { useNavigate } from "react-router-dom";
 import "./Success.css";
 import Success_gif from "../Assets/gif/payment succesful png.gif";
 import { Assets } from "../Assets/Assets";
+import { useCount } from "../../Context/Context";
 
 const Success = () => {
   const navigate = useNavigate();
   const [isSuccess, setIsSuccess] = useState();
+  const { count } = useCount();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -23,11 +25,17 @@ const Success = () => {
           <img src={Assets.Congrats} alt="" />
         </div>
       )}
+
       {!isSuccess && (
         <div className="img-gif">
           <img src={Success_gif} alt="" />
         </div>
       )}
+
+      <div className="win">
+        <span>You Have Won</span>
+        <h3>{count} Coupens</h3>
+      </div>
       {isSuccess && (
         <div className="tickets">
           <img src={Assets.ticket} alt="" />
